780bc12fcb1dfc7e01f4cd0c6b4d26ac71b0d018
Vannsl <boehner.vanessa@gmail.com>
2019-12-29 13:48:24 +0100

    changes font color of navigation bar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -41,7 +41,7 @@
   }
 </style>
 
-<nav class="px-1 font-normal">
+<nav class="px-1 font-normal text-primary">
   <ul class="m-0 p-0">
     <li>
       <a class:selected="{segment === undefined}" href="." rel="prefetch"
