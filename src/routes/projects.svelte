0fe9964d0835043cb14eb8691af77af485cd81b7
Vannsl <boehner.vanessa@gmail.com>
2020-01-05 14:30:37 +0100

    minor styling for coming soon section

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -2,6 +2,8 @@
   <title>Vannsl Projects</title>
 </svelte:head>
 
-<section class="h-screen">
-  Coming Soon
+<section class="h-screen pt-4">
+  <p class="text-center">
+    Coming Soon
+  </p>
 </section>
