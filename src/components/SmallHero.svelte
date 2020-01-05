312a6070190dd49cf42d2e0152103f1db72f90b6
Vannsl <boehner.vanessa@gmail.com>
2020-01-05 15:51:24 +0100

    fixes styling for mobile devices

--- src/components/SmallHero.svelte
+++ src/components/SmallHero.svelte
@@ -3,10 +3,12 @@
   export let extension = "jpg";
   export let type;
   export let background =
-    type === "contain" ? "sm-only:bg-center bg-contain" : "bg-top bg-cover";
+    type === "contain"
+      ? "sm-only:bg-center bg-contain bg-fixed"
+      : "sm:bg-top bg-center bg-cover sm:bg-fixed";
 </script>
 
 <section
-  class="w-full p-10 sm:p-16 md:p-32 lg:p-40 xl:p-64 bg-fixed {background}"
+  class="w-full p-10 sm:p-16 md:p-32 lg:p-40 xl:p-64 {background}"
   style="background-image: url('vannsl-{ segment }.{extension}')"
 ></section>
