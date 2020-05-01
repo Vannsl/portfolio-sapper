76897d9dea92b4772653e5e186282f8e2e5e9e7a
Vannsl <boehner.vanessa@gmail.com>
2020-05-01 17:19:15 +0200

    chore: minifies css on dev and prod

--- postcss.config.js
+++ postcss.config.js
@@ -9,6 +9,10 @@ module.exports = {
   plugins: [
     tailwindcss("./tailwind.config.js"),
 
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+
+    require('cssnano')({
+      preset: 'default',
+    }),
   ]
 };
