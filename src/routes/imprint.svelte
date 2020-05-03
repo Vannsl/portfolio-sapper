13879405353fd96196d3c44557d4eafd8339005f
Vannsl <boehner.vanessa@gmail.com>
2020-05-03 14:09:19 +0200

    feat: full german imprint

---
+++ src/routes/imprint.svelte
@@ -0,0 +1,100 @@
+<script>
+  import { fadeIn, fadeOut } from "../utils/page_fade";
+  import HeadlineSecondary from "../components/HeadlineSecondary.svelte";
+  import Spacing from "../components/Spacing.svelte";
+</script>
+
+<svelte:head>
+  <title>Vannsl | Imprint</title>
+</svelte:head>
+
+<article in:fadeIn out:fadeOut class="bg-secondary">
+  <header class="w-full p-10 pt-24 sm:p-16 md:p-32 lg:p-40 xl:p-64 bg-primary">
+    <h1 class="text-center text-white text-xl md:text-6xl">
+      Imprint / Impressum
+    </h1>
+  </header>
+  <div class="max-w-screen-md mx-auto py-16">
+    <section class="mb-4">
+      <HeadlineSecondary>Angaben gemäß § 5 TMG</HeadlineSecondary>
+      <p>
+        Vanessa Böhner<br />
+        Egilbertstr. 13<br />
+        85354 Freising
+      </p>
+    </section>
+    <section class="mb-4">
+      <HeadlineSecondary>Kontakt</HeadlineSecondary>
+      <p>
+        E-Mail: mail@expect-exception.fail
+      </p>
+    </section>
+    <section class="mb-4">
+      <HeadlineSecondary>Haftung für Inhalte</HeadlineSecondary>
+      <p>
+        Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf
+        diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8
+        bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet,
+        übermittelte oder gespeicherte fremde Informationen zu überwachen oder
+        nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit
+        hinweisen.
+      </p>
+    </section>
+    <section class="mb-4">
+      <HeadlineSecondary>Haftung für Inhalte</HeadlineSecondary>
+      <p>
+        Als Diensteanbieter sind wir gem&äß § 7 Abs.1 TMG für eigene Inhalte auf
+        diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8
+        bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet,
+        übermittelte oder gespeicherte fremde Informationen zu überwachen oder
+        nach Umst#nden zu forschen, die auf eine rechtswidrige ätigkeit
+        hinweisen.
+      </p>
+      <p>
+        Verpflichtungen zur Entfernung oder Sperrung der Nutzung von
+        Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.
+        Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der
+        Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von
+        entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend
+        entfernen.
+      </p>
+    </section>
+    <section class="mb-4">
+      <HeadlineSecondary>Haftung für Links</HeadlineSecondary>
+      <p>
+        Unser Angebot enthält Links zu externen Websites Dritter, auf deren
+        Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden
+        Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten
+        Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten
+        verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der
+        Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte
+        waren zum Zeitpunkt der Verlinkung nicht erkennbar.
+      </p>
+      <p>
+        Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch
+        ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
+        Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend
+        entfernen.
+      </p>
+    </section>
+    <section class="mb-4">
+      <HeadlineSecondary>Urheberrecht</HeadlineSecondary>
+      <p>
+        Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
+        Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung,
+        Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
+        Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des
+        jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite
+        sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
+      </p>
+      <p>
+        Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden,
+        werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte
+        Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
+        Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
+        entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden
+        wir derartige Inhalte umgehend entfernen.
+      </p>
+    </section>
+  </div>
+</article>
