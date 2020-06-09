8c41d7a471143e4e74028f50a2dad7e70988f640
Vannsl <boehner.vanessa@gmail.com>
2020-06-09 15:52:04 +0200

    feat: add blacklivesmatter banner

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -65,46 +65,51 @@
 
 <svelte:window bind:scrollY={y} />
 
-<nav class="{y > 8 ? 'fixed shadow-lg bg-white dark:bg-dark text-tertiary dark:text-tertiary-light' : 'absolute mt-2 text-white p-2 sm:px-8 sm:py-4 sm:text-lg'} trans p-0 w-full z-10 flex justify-between uppercase tracking-wider  {y > 8 || segment === undefined || segment === 'imprint' ? '' : 'bg-dark bg-opacity-75 sm:bg-opacity-0'} text-sm sm:text-base">
-  <ul class={listClasses}>
-    <li class="{itemClasses} md:ml-2">
-      <a
-        class:selected={segment === undefined}
-        href="."
-        rel="prefetch"
-        class={linkClasses}
-      >
-        vannsl
-      </a>
-    </li>
-  </ul>
-
-  <ul class={listClasses}>
-    {#each items as item}
-      <li class={itemClasses}>
+<nav class="{y > 8 ? 'fixed shadow-lg' : 'absolute mt-2'} w-full z-10 uppercase tracking-wider text-sm sm:text-base">
+  <div class="p-1 text-sm text-center bg-dark text-white">
+    #blacklivesmatter
+  </div>
+  <div class="{y > 8 ? 'bg-white dark:bg-dark text-tertiary dark:text-tertiary-light' : 'text-white p-2 sm:px-8 sm:py-4 sm:text-lg'} trans p-0 flex justify-between {y > 8 || segment === undefined || segment === 'imprint' ? '' : 'bg-dark bg-opacity-75 sm:bg-opacity-0'}">
+    <ul class="{listClasses}">
+      <li class="{itemClasses} md:ml-2">
         <a
-          class:selected={segment === item}
-          href={item}
+          class:selected={segment === undefined}
+          href="."
           rel="prefetch"
           class={linkClasses}
         >
-          {item}
+          vannsl
         </a>
       </li>
-    {/each}
-    <li class={itemClasses}>
-      <button
-        aria-label="Activate dark mode"
-        class:invisible={isLoading}
-        class="mt-4 mx-2 sm:mx-4 w-5 sm:w-6 cursor-pointer opacity-50 hover:opacity-100"
-        on:click={toggleMode}
-      >
-        {#if darkMode}
-          <Sun></Sun>
-        {:else}
-          <Moon></Moon>
-        {/if}
-      </button>
-    </li>
-  </ul>
+    </ul>
+
+    <ul class={listClasses}>
+      {#each items as item}
+        <li class={itemClasses}>
+          <a
+            class:selected={segment === item}
+            href={item}
+            rel="prefetch"
+            class={linkClasses}
+          >
+            {item}
+          </a>
+        </li>
+      {/each}
+      <li class={itemClasses}>
+        <button
+          aria-label="Activate dark mode"
+          class:invisible={isLoading}
+          class="mt-4 mx-2 sm:mx-4 w-5 sm:w-6 cursor-pointer opacity-50 hover:opacity-100"
+          on:click={toggleMode}
+        >
+          {#if darkMode}
+            <Sun></Sun>
+          {:else}
+            <Moon></Moon>
+          {/if}
+        </button>
+      </li>
+    </ul>
+  </div>
 </nav>
