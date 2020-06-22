ad583f7c8e668106addae206f03a2c1ec92a4225
Vannsl <boehner.vanessa@gmail.com>
2020-06-22 09:56:49 +0200

    chore: add podcasts and publications

--- src/routes/speaking.svelte
+++ src/routes/speaking.svelte
@@ -14,12 +14,13 @@
     {
       title: "[CANCELLED] Testing Techniques for Component Driven Development",
       event: "Think About!",
-      eventSrc: "https://think-about.io/events/2020/talks/component_driven_development.html",
+      eventSrc:
+        "https://think-about.io/events/2020/talks/component_driven_development.html",
       location: "Cologne",
       date: "04/06/2020",
-      emoji: "germany"
-    }
-  ]
+      emoji: "germany",
+    },
+  ];
 
   export let speakingPast = [
     {
@@ -29,16 +30,17 @@
       location: "Munich",
       slidesSrc: "https://slides.com/vannsl/microfrontends#/",
       date: "21/03/2020",
-      emoji: "germany"
+      emoji: "germany",
     },
     {
       title: "Microfrontends: The Good, The Bad and The Ugly",
       event: "Code is Colorful",
-      eventSrc: "https://www.meetup.com/de-DE/SinnerSchrader-Frankfurt/events/268136278/",
+      eventSrc:
+        "https://www.meetup.com/de-DE/SinnerSchrader-Frankfurt/events/268136278/",
       location: "Frankfurt",
       slidesSrc: "https://slides.com/vannsl/microfrontends#/",
       date: "25/02/2020",
-      emoji: "germany"
+      emoji: "germany",
     },
     {
       title: "Microfrontends: The Good, The Bad and The Ugly",
@@ -47,16 +49,17 @@
       location: "Prague",
       slidesSrc: "https://slides.com/vannsl/microfrontends#/",
       date: "28/01/2020",
-      emoji: "czech-republic"
+      emoji: "czech-republic",
     },
     {
       title: "Microfrontends: The Good, The Bad and The Ugly",
       event: "Code is Colorful",
-      eventSrc: "https://www.meetup.com/de-DE/SinnerSchrader-Munich-Events/events/267755040/",
+      eventSrc:
+        "https://www.meetup.com/de-DE/SinnerSchrader-Munich-Events/events/267755040/",
       location: "Munich",
       slidesSrc: "https://slides.com/vannsl/microfrontends#/",
       date: "22/01/2020",
-      emoji: "germany"
+      emoji: "germany",
     },
     {
       title: "Microfrontends: The Good, The Bad and The Ugly",
@@ -65,7 +68,7 @@
       location: "Hamburg",
       slidesSrc: "https://slides.com/vannsl/microfrontends#/",
       date: "11/12/2019",
-      emoji: "germany"
+      emoji: "germany",
     },
     {
       title: "Accidentally JAM Stack",
@@ -74,7 +77,7 @@
       location: "Linz",
       videoSrc: "https://www.youtube.com/watch?v=G20P46_4L4A",
       date: "24/10/2019",
-      emoji: "austria"
+      emoji: "austria",
     },
     {
       title: "Why I decided to speak at Conferences",
@@ -83,7 +86,7 @@
       location: "Munich",
       slidesSrc: "https://slides.com/vannsl/public-speaking#/",
       date: "20/09/2019",
-      emoji: "germany"
+      emoji: "germany",
     },
     {
       title: "Adventure Time: Let's build a website in 2019",
@@ -92,7 +95,7 @@
       location: "Nuremberg",
       slidesSrc: "https://slides.com/vannsl/adventure-time-franken-js#/",
       date: "29/07/2019",
-      emoji: "germany"
+      emoji: "germany",
     },
     {
       title: "Server-Side Rendering using Nuxt.js",
@@ -101,23 +104,25 @@
       location: "Dornbirn",
       slidesSrc: "https://slides.com/vannsl/workerconf-nuxt#/",
       date: "28/06/2019",
-      emoji: "austria"
+      emoji: "austria",
     },
     {
       title: "Vue.js: Entstehung, Einsatzgebiete und Zukunftsprognosen",
       event: "enterJS",
-      eventSrc: "https://enterjs.de/2019/single?id=8374&vue.js%3A-entstehung%2C-einsatzgebiete-und-zukunftsprognosen",
+      eventSrc:
+        "https://enterjs.de/2019/single?id=8374&vue.js%3A-entstehung%2C-einsatzgebiete-und-zukunftsprognosen",
       location: "Darmstadt",
       date: "26/06/2019",
-      emoji: "germany"
+      emoji: "germany",
     },
     {
       title: "From Legacy to Vue.js",
       event: "iJS International JavaScript Conference",
-      eventSrc: "https://javascript-conference.com/vue-js/from-legacy-to-vue-js/",
+      eventSrc:
+        "https://javascript-conference.com/vue-js/from-legacy-to-vue-js/",
       location: "London",
       date: "14/05/2019",
-      emoji: "united-kingdom"
+      emoji: "united-kingdom",
     },
     {
       title: "Lightning: Nuxt.js <3 TailwindCSS",
@@ -125,50 +130,59 @@
       eventSrc: "https://frontconf.com/",
       location: "Munich",
       date: "27/04/2019",
-      emoji: "germany"
+      emoji: "germany",
     },
     {
-      title: "The VUE(.js)ture is here – endlich erweiter- und wartbare Webapplikationen?",
+      title:
+        "The VUE(.js)ture is here – endlich erweiter- und wartbare Webapplikationen?",
       event: "JavaScript Days",
-      eventSrc: "https://javascript-days.de/javascript/the-vue-jsture-is-here-endlich-erweiter-und-wartbare-webapplikationen/",
+      eventSrc:
+        "https://javascript-days.de/javascript/the-vue-jsture-is-here-endlich-erweiter-und-wartbare-webapplikationen/",
       location: "Munich",
       date: "20/03/2019",
-      emoji: "germany"
+      emoji: "germany",
     },
     {
-      title: "How to build a static website in two and a half days with Nuxt and TailwindCSS",
+      title:
+        "How to build a static website in two and a half days with Nuxt and TailwindCSS",
       event: "TechDevision Tech Talk",
       eventSrc: "https://www.techdivision.com/unternehmen/veranstaltungen.html",
       location: "Munich",
       date: "21/02/2019",
-      emoji: "germany"
+      emoji: "germany",
     },
     {
       title: "New website in two and a half days",
       event: "Code is Colorful",
-      eventSrc: "https://www.meetup.com/de-DE/sinnerschrader-hamburg/events/256937241/",
+      eventSrc:
+        "https://www.meetup.com/de-DE/sinnerschrader-hamburg/events/256937241/",
       location: "Hamburg",
       date: "07/02/2019",
-      emoji: "germany"
+      emoji: "germany",
     },
     {
-      title: "How to build a static website in two and a half days with Nuxt and TailwindCSS",
+      title:
+        "How to build a static website in two and a half days with Nuxt and TailwindCSS",
       event: "Frontend Developers",
-      eventSrc: "https://www.meetup.com/de-DE/Munich-Frontend-Developers/events/256146257/",
+      eventSrc:
+        "https://www.meetup.com/de-DE/Munich-Frontend-Developers/events/256146257/",
       location: "Munich",
       date: "22/11/2018",
-      codebases: [{
-        src: "https://github.com/Vannsl/ausbaufaehig-podcast"
-      }],
-      emoji: "germany"
+      codebases: [
+        {
+          src: "https://github.com/Vannsl/ausbaufaehig-podcast",
+        },
+      ],
+      emoji: "germany",
     },
     {
       title: "Vue Single File Components and Composing",
       event: "TechDevision Tech Talk",
-      eventSrc: "https://www.meetup.com/de-DE/TechDivision-TechTalk-Munchen/events/254404298/",
+      eventSrc:
+        "https://www.meetup.com/de-DE/TechDivision-TechTalk-Munchen/events/254404298/",
       location: "Munich",
       date: "20/09/2018",
-      emoji: "germany"
+      emoji: "germany",
     },
     {
       title: "Vue.js Component Tools",
@@ -177,10 +191,12 @@
       location: "Munich",
       date: "08/08/2018",
       slidesSrc: "https://de.slideshare.net/VanessaBhner/vuejs-component-tools",
-      codebases: [{
-        src: "https://github.com/Vannsl/Vue-Composing-Components"
-      }],
-      emoji: "germany"
+      codebases: [
+        {
+          src: "https://github.com/Vannsl/Vue-Composing-Components",
+        },
+      ],
+      emoji: "germany",
     },
     {
       title: "Pain Free Frontend Development",
@@ -189,120 +205,147 @@
       location: "Munich",
       date: "30/06/2018",
       slidesSrc: "https://de.slideshare.net/VanessaBhner/spa-flask-vue",
-      codebases: [{
-        src: "https://github.com/Vannsl/spa-flask-vue"
-      }],
-      emoji: "germany"
+      codebases: [
+        {
+          src: "https://github.com/Vannsl/spa-flask-vue",
+        },
+      ],
+      emoji: "germany",
     },
     {
       title: "State Transitions with Vue.js",
       event: "myposter Tech 'n' Drinks",
-      eventSrc: "https://team.myposter.de/beer-bbq-vue-js-beim-tech-n-drinks-myposter/",
+      eventSrc:
+        "https://team.myposter.de/beer-bbq-vue-js-beim-tech-n-drinks-myposter/",
       location: "Munich",
       date: "12/06/2018 I",
-      slidesSrc: "https://www.slideshare.net/myposter_techtalks/vue-state-transitions",
+      slidesSrc:
+        "https://www.slideshare.net/myposter_techtalks/vue-state-transitions",
       codebases: [
         {
-          src: "https://github.com/Vannsl/Vue-Transitions-Animations"
+          src: "https://github.com/Vannsl/Vue-Transitions-Animations",
         },
         {
-          src: "https://github.com/Vannsl/vue-router-transitions"
-        }
+          src: "https://github.com/Vannsl/vue-router-transitions",
+        },
       ],
-      emoji: "germany"
+      emoji: "germany",
     },
     {
       title: "Composing Components",
       event: "myposter Tech 'n' Drinks",
-      eventSrc: "https://team.myposter.de/beer-bbq-vue-js-beim-tech-n-drinks-myposter/",
+      eventSrc:
+        "https://team.myposter.de/beer-bbq-vue-js-beim-tech-n-drinks-myposter/",
       location: "Munich",
       date: "12/06/2018 II",
-      slidesSrc: "https://www.slideshare.net/myposter_techtalks/vue-composing-components",
-      codebases: [{
-        src: "https://github.com/Vannsl/Vue-Composing-Components"
-      }],
-      emoji: "germany"
+      slidesSrc:
+        "https://www.slideshare.net/myposter_techtalks/vue-composing-components",
+      codebases: [
+        {
+          src: "https://github.com/Vannsl/Vue-Composing-Components",
+        },
+      ],
+      emoji: "germany",
     },
-  ]
+  ];
 
-export let podcasts = [
-  {
-    title: "Svelte",
-    src: "https://workingdraft.de/413/",
-    publisher: "Working Draft",
-    episode: "Revision 413",
-    date: "18/02/2020",
-    emoji: "podcast"
-  },
-  {
-    title: "Micro Frontends mit Project Mosaic",
-    src: "https://workingdraft.de/407/",
-    publisher: "Working Draft",
-    episode: "Revision 407",
-    date: "07/01/2020",
-    emoji: "podcast"
-  },
-  {
-    title: "Frontend Testing",
-    src: "https://workingdraft.de/375/",
-    publisher: "Working Draft",
-    episode: "Revision 375",
-    date: "11/02/2019",
-    emoji: "podcast"
-  },
-  {
-    title: "Vue.js - Der Deep Dive",
-    src: "https://workingdraft.de/367/",
-    publisher: "Working Draft",
-    episode: "Revision 367",
-    date: "29/11/2018",
-    emoji: "podcast"
-  }
-]
+  export let podcasts = [
+    {
+      title: "Svelte",
+      src:
+        "https://programmierbar.buzzsprout.com/176239/4238645-folge-64-svelte-mit-vanessa-bohner",
+      publisher: "programmierbar",
+      episode: "Folge 64",
+      date: "19/06/2020",
+      emoji: "podcast",
+    },
+    {
+      title: "Cypress: E2E Testing",
+      src: "https://expect-exception.fail/podcast/1",
+      publisher: "expect(Exception)",
+      episode: "Episode 1",
+      date: "15/05/2020",
+      emoji: "podcast",
+    },
+    {
+      title: "Svelte",
+      src: "https://workingdraft.de/413/",
+      publisher: "Working Draft",
+      episode: "Revision 413",
+      date: "18/02/2020",
+      emoji: "podcast",
+    },
+    {
+      title: "Micro Frontends mit Project Mosaic",
+      src: "https://workingdraft.de/407/",
+      publisher: "Working Draft",
+      episode: "Revision 407",
+      date: "07/01/2020",
+      emoji: "podcast",
+    },
+    {
+      title: "Frontend Testing",
+      src: "https://workingdraft.de/375/",
+      publisher: "Working Draft",
+      episode: "Revision 375",
+      date: "11/02/2019",
+      emoji: "podcast",
+    },
+    {
+      title: "Vue.js - Der Deep Dive",
+      src: "https://workingdraft.de/367/",
+      publisher: "Working Draft",
+      episode: "Revision 367",
+      date: "29/11/2018",
+      emoji: "podcast",
+    },
+  ];
 
-export let interviews = [
-  {
-    title: "Vue.js: Nicht nur für neue Projekte eine gute Wahl | Vanessa Böhner im Interview | JS Days 2019",
-    src: "https://www.youtube.com/watch?v=-tm2o4NHme4",
-    publisher: "EntwickerAkadamie",
-    date: "12/06/2019",
-    emoji: "videointerviews"
-  }
-]
+  export let interviews = [
+    {
+      title:
+        "Vue.js: Nicht nur für neue Projekte eine gute Wahl | Vanessa Böhner im Interview | JS Days 2019",
+      src: "https://www.youtube.com/watch?v=-tm2o4NHme4",
+      publisher: "EntwickerAkadamie",
+      date: "12/06/2019",
+      emoji: "videointerviews",
+    },
+  ];
 
-export let mentoring = [
-  {
-    title: "Vue Vixens Full Day Workshop",
-    event: "ScriptConf",
-    eventSrc: "https://scriptconf.org/#workshops",
-    location: "Linz",
-    date: "24/10/2019",
-    emoji: "mentoring"
-  },
-  {
-    title: "Introduction to Nuxt.js",
-    event: "workerConf",
-    eventSrc: "https://scriptconf.org/#workshops",
-    location: "Bregenz",
-    date: "27/06/2019",
-    slidesSrc: "https://slides.com/vannsl/nuxtjs-harry-potter#/",
-    emoji: "mentoring"
-  },
-  {
-    title: "JSDay - half a day full of JavaScript",
-    event: "JSKongress Pre-Event @ SinnerSchrader",
-    eventSrc: "https://www.meetup.com/de-DE/SinnerSchrader-Munich-Events/events/257780477/",
-    location: "Munich",
-    date: "10/03/2019",
-    emoji: "mentoring"
-  },
-  {
-    title: "ngGirls Full Day Workshop",
-    location: "Munich",
-    date: "24/11/2018",
-    emoji: "mentoring"
-  }
-]
+  export let mentoring = [
+    {
+      title: "Vue Vixens Full Day Workshop",
+      event: "ScriptConf",
+      eventSrc: "https://scriptconf.org/#workshops",
+      location: "Linz",
+      date: "24/10/2019",
+      emoji: "mentoring",
+    },
+    {
+      title: "Introduction to Nuxt.js",
+      event: "workerConf",
+      eventSrc: "https://scriptconf.org/#workshops",
+      location: "Bregenz",
+      date: "27/06/2019",
+      slidesSrc: "https://slides.com/vannsl/nuxtjs-harry-potter#/",
+      emoji: "mentoring",
+    },
+    {
+      title: "JSDay - half a day full of JavaScript",
+      event: "JSKongress Pre-Event @ SinnerSchrader",
+      eventSrc:
+        "https://www.meetup.com/de-DE/SinnerSchrader-Munich-Events/events/257780477/",
+      location: "Munich",
+      date: "10/03/2019",
+      emoji: "mentoring",
+    },
+    {
+      title: "ngGirls Full Day Workshop",
+      location: "Munich",
+      date: "24/11/2018",
+      emoji: "mentoring",
+    },
+  ];
 </script>
 
 <svelte:head>
@@ -310,29 +353,8 @@ export let mentoring = [
 </svelte:head>
 
 <main in:fadeIn out:fadeOut>
-  <SmallHero segment="speaking" type="contain" />
-  <Header title="talks" />
-  <Spacing></Spacing>
-  <Article>
-    <HeadlineTertiary>upcoming</HeadlineTertiary>
-    <div class="mb-4 p-4 border-2 border-dashed border-primary bg-dark text-white">
-      <p>Due to COVID-19 I won't attend any conferences, workshops or MeetUps (unless fully remote) <s>for 2 months</s> until it's over.</p>
-    </div>
-    <List>
-      {#each speakingUpcoming as item (item.date)}
-        <ListItem {item} />
-      {/each}
-    </List>
-    <HeadlineTertiary>past</HeadlineTertiary>
-    <List>
-      {#each speakingPast as item (item.date)}
-        <ListItem {item} />
-      {/each}
-    </List>
-  </Article>
-  <Spacing></Spacing>
   <Header title="podcasts & interviews" />
-  <Spacing></Spacing>
+  <Spacing />
   <Article>
     <HeadlineTertiary>podcasts</HeadlineTertiary>
     <List>
@@ -349,9 +371,36 @@ export let mentoring = [
       {/each}
     </List>
   </Article>
-  <Spacing></Spacing>
+  <Spacing />
+  <SmallHero segment="speaking" type="contain" />
+  <Header title="talks" />
+  <Spacing />
+  <Article>
+    <HeadlineTertiary>upcoming</HeadlineTertiary>
+    <div
+      class="mb-4 p-4 border-2 border-dashed border-primary bg-dark text-white">
+      <p>
+        Due to COVID-19 I won't attend any conferences, workshops or MeetUps
+        (unless fully remote)
+        <s>for 2 months</s>
+        until it's over.
+      </p>
+    </div>
+    <List>
+      {#each speakingUpcoming as item (item.date)}
+        <ListItem {item} />
+      {/each}
+    </List>
+    <HeadlineTertiary>past</HeadlineTertiary>
+    <List>
+      {#each speakingPast as item (item.date)}
+        <ListItem {item} />
+      {/each}
+    </List>
+  </Article>
+  <Spacing />
   <Header title="mentoring & workshops" />
-  <Spacing></Spacing>
+  <Spacing />
   <Article>
     <List>
       {#each mentoring as item (item.date)}
@@ -360,6 +409,6 @@ export let mentoring = [
     </List>
   </Article>
 
-  <Spacing></Spacing>
-  <Spacing></Spacing>
+  <Spacing />
+  <Spacing />
 </main>
--- src/routes/writing.svelte
+++ src/routes/writing.svelte
@@ -9,80 +9,99 @@
   import Spacing from "../components/Spacing.svelte";
 
   export let publications = [
-   {
-      title: "Configuring TailwindCSS for Sapper",
-      src: "https://dev.to/vannsl/configuring-tailwindcss-for-sapper-58jm",
+    {
+      title: "Tips for remote goodbyes",
+      src: "https://dev.to/vannsl/tips-for-remote-goodbyes-2o15",
       publisher: "dev.to",
-      date: "31/01/2020",
-      emoji: "publications"
+      date: "22/06/2020",
+      emoji: "publications",
     },
-   {
-      title: "All you need to know to start writing Svelte Single File Components",
-      src: "https://dev.to/vannsl/all-you-need-to-know-to-start-writing-svelte-single-file-components-cbd",
+    {
+      title: "Playwright (vs. Puppeteer): Cross-Browser Testing done right",
+      src:
+        "https://dev.to/vannsl/playwright-vs-puppeteer-cross-browser-testing-done-right-4e3",
+      publisher: "dev.to",
+      date: "24/05/2020",
+      emoji: "publications",
+    },
+    {
+      title:
+        "All you need to know to start writing Svelte Single File Components",
+      src:
+        "https://dev.to/vannsl/all-you-need-to-know-to-start-writing-svelte-single-file-components-cbd",
       publisher: "dev.to",
       date: "17/01/2020",
-      emoji: "publications"
+      emoji: "publications",
     },
     {
       title: "Statically generated website with Svelte and Sapper",
-      src: "https://dev.to/vannsl/statically-generated-website-with-svelte-and-sapper-5bi7",
+      src:
+        "https://dev.to/vannsl/statically-generated-website-with-svelte-and-sapper-5bi7",
       publisher: "dev.to",
       date: "05/01/2020",
-      emoji: "publications"
+      emoji: "publications",
     },
     {
       title: "Why it took me three weeks to implement a Login Form",
-      src: "https://dev.to/vannsl/why-it-took-me-three-weeks-to-implement-a-login-form-3aa8",
+      src:
+        "https://dev.to/vannsl/why-it-took-me-three-weeks-to-implement-a-login-form-3aa8",
       publisher: "dev.to",
       date: "19/11/2019",
-      emoji: "publications"
+      emoji: "publications",
     },
     {
       title: "E-Mail/Password Authentication with Firebase and Nuxt",
-      src: "https://dev.to/vannsl/e-mail-password-authentication-with-firebase-and-nuxt-292m",
+      src:
+        "https://dev.to/vannsl/e-mail-password-authentication-with-firebase-and-nuxt-292m",
       publisher: "helpdev",
       date: "19/10/2019",
-      emoji: "publications"
+      emoji: "publications",
     },
     {
-      title: "Einführung in Vue.js: Was steckt wirklich hinter dem progressiven Framework?",
-      src: "https://www.hosteurope.de/blog/einfuehrung-in-vue-js-was-steckt-wirklich-hinter-dem-progressiven-framework/",
+      title:
+        "Einführung in Vue.js: Was steckt wirklich hinter dem progressiven Framework?",
+      src:
+        "https://www.hosteurope.de/blog/einfuehrung-in-vue-js-was-steckt-wirklich-hinter-dem-progressiven-framework/",
       publisher: "Host Europe",
       date: "13/05/2019",
-      emoji: "publications"
-    }
-  ]
+      emoji: "publications",
+    },
+  ];
 
   export let interviews = [
     {
       title: "Being part of JS Kongress Program Committee",
-      src: "https://dev.to/s2engineers/being-part-of-js-kongress-program-committee-2mal",
+      src:
+        "https://dev.to/s2engineers/being-part-of-js-kongress-program-committee-2mal",
       publisher: "SinnerSchrader Engineers",
       date: "12/03/2020",
-      emoji: "interviews"
+      emoji: "interviews",
     },
     {
       title: "New Member for the PC: Vanessa Böhner",
-      src: "https://medium.com/@jskongress/new-member-for-the-pc-vanessa-b%C3%B6hner-b64bc7ef6f83",
+      src:
+        "https://medium.com/@jskongress/new-member-for-the-pc-vanessa-b%C3%B6hner-b64bc7ef6f83",
       publisher: "JS Kongress Munich",
       date: "16/09/2019",
-      emoji: "interviews"
+      emoji: "interviews",
     },
     {
       title: "Vue.js: A mix of Angular and React with MVVM and Virtual DOM",
-      src: "https://helpdev.eu/vue-js-a-mix-of-angular-and-react-with-mvvm-and-virtual-dom/",
+      src:
+        "https://helpdev.eu/vue-js-a-mix-of-angular-and-react-with-mvvm-and-virtual-dom/",
       publisher: "helpdev",
       date: "29/01/2019 I",
-      emoji: "interviews"
+      emoji: "interviews",
     },
     {
       title: "Vue.js: Ein Mix aus Angular und React mit MVVM und Virtual DOM",
-      src: "https://javascript-days.de/blog/vue-js-ein-mix-aus-angular-und-react-mit-mvvm-und-virtual-dom/",
+      src:
+        "https://javascript-days.de/blog/vue-js-ein-mix-aus-angular-und-react-mit-mvvm-und-virtual-dom/",
       publisher: "JS Days",
       date: "29/01/2019 II",
-      emoji: "interviews"
-    }
-  ]
+      emoji: "interviews",
+    },
+  ];
 </script>
 
 <svelte:head>
@@ -92,7 +111,7 @@
 <main in:fadeIn out:fadeOut>
   <SmallHero segment="writing" type="cover" />
   <Header title="publications" />
-  <Spacing></Spacing>
+  <Spacing />
   <Article>
     <List>
       {#each publications as item (item.date)}
@@ -100,9 +119,9 @@
       {/each}
     </List>
   </Article>
-  <Spacing></Spacing>
+  <Spacing />
   <Header title="interviews" />
-  <Spacing></Spacing>
+  <Spacing />
   <Article>
     <List type="interviews">
       {#each interviews as item (item.date)}
@@ -111,6 +130,6 @@
     </List>
   </Article>
 
-  <Spacing></Spacing>
-  <Spacing></Spacing>
+  <Spacing />
+  <Spacing />
 </main>
