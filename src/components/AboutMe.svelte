38e1bf0b150902ef9858057bbaa14640f82f7781
Vannsl <boehner.vanessa@gmail.com>
2020-07-08 19:21:10 +0200

    feat: add link

--- src/components/AboutMe.svelte
+++ src/components/AboutMe.svelte
@@ -13,15 +13,18 @@
     <p class="md:px-4 text-tertiary dark:text-tertiary-light">
       Hi, nice to meet you!<br /><br />My name is Vanessa. I am a Web
       Developer. My nickname is Vannsl since I lost various passwords for Vanny in 1998.<br /><br />
-      I'm a Frontend Foxes Chapter Leader,
+      I'm a <OutgoingLink href="https://www.vuevixens.org/">Frontend Foxes</OutgoingLink> Chapter Leader,
       helping others with their first coding and speaking experiences while
       learning from them, too. In 2020 I started the Frontend Testing Podcast
       <OutgoingLink href="https://www.expect-exception.fail">expect(Exception)</OutgoingLink>.
       Also in 2020, I was member of the JS Kongress Program Committee. I work as a Software Developer at
+      <OutgoingLink href="https://www.zage.life/">ZAGE</OutgoingLink> by
       <OutgoingLink href="https://www.thelifecompany.com/">The Life Company</OutgoingLink>.
       From 2010-2015 I studied Media Informatics with a focus on Human-Computer-Interaction. Now I am striving to
       provide customers with the best experience possible while creating modern
-      and accessible web applications. When I writes code, she do it not for myself but the next developer.<br><br>
+      and accessible web applications. When I writes code, she do it not for myself but the next developer.
+      In my spare time I like to do some sports and drink (for scientific purposes) wine on my Wine Podcast 
+      <OutgoingLink href="https://www.ausbaufaehig-podcast.de/">Ausbaufähig</OutgoingLink>.<br><br>
       Feel free to send me a message on <OutgoingLink href="https://twitter.com/vannsl">Twitter</OutgoingLink>.
     </p>
     <Spacing></Spacing>
